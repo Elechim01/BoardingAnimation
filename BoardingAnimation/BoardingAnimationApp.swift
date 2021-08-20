b37a67a3fc21cf3ec1baff5b8c870062a6a9b872
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-08-20 23:02:47 +0200

    Initial Commit

---
+++ BoardingAnimation/BoardingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoardingAnimationApp.swift
+//  BoardingAnimation
+//
+//  Created by Michele Manniello on 20/08/21.
+//
+
+import SwiftUI
+
+@main
+struct BoardingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
